8a9b903d6f054323f3fa965960ce6c74b8ae55c1
saul-manich <saulmamani73@gmail.com>
2016-08-21 23:32:53 -0400

    CantidadCerosConsola

---
+++ cuantosCEROS/cantidad de ceros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cantidad_de_ceros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, num, dig, x;
+            c = 0;
+            x = 0;
+            Console.WriteLine("introdusca el valor");
+            num = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                c++;
+                dig = num % 10;
+                if (dig == 0)
+                {
+                    x = x + 1;
+                }
+               
+                num = num / 10;
+            } while (num != 0);
+            Console.WriteLine("la cantidad de ceros son " + x);
+            Console.ReadKey();
+        }
+    }
+}
